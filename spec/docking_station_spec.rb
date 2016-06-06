e24d3407b10e5dbf5bc3020b792e195bae8ada0c
Finble <elainefinn@hotmail.com>
2016-06-06 16:10:18 +0100

    Release bike method updated to include fail message if no bikes, and to refer to existing bikes via instance variables vs infinite bikes via Bike.new

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,10 +4,10 @@ describe DockingStation do
 
 	it {is_expected.to respond_to :release_bike}
 
-	it 'releases working bikes' do
-		bike = subject.release_bike
-		expect(bike).to be_working
-	end
+	# it 'releases working bikes' do
+	# 	bike = subject.release_bike
+	# 	expect(bike).to be_working
+	# end
 
 	it {is_expected.to respond_to(:dock).with(1).argument}
 
@@ -18,5 +18,18 @@ describe DockingStation do
 		subject.dock(bike)
 		expect(subject.bike).to eq bike
 	end
-	
+
+		describe '#release_bike' do
+
+			it 'releases a bike' do
+			bike = Bike.new
+			subject.dock(bike)
+			expect(subject.release_bike).to eq bike
+			end
+
+			it 'raises an error where there are no bikes available' do
+			expect {subject.release_bike}.to raise_error 'No bikes available'
+			end
+
+		end
 end
