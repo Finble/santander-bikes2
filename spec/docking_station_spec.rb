b528f817dcab697dbef139cb44f3d7b0345854ed
Finble <elainefinn@hotmail.com>
2016-06-06 16:48:58 +0100

    Limited capacity and changed instance variable into array

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,11 +13,11 @@ describe DockingStation do
 
 	it {is_expected.to respond_to(:bike)}
 
-	it 'returns docked bikes' do
-		bike = Bike.new
-		subject.dock(bike)
-		expect(subject.bike).to eq bike
-	end
+	# it 'returns docked bikes' do
+	# 	bike = Bike.new
+	# 	subject.dock(bike)
+	# 	expect(subject.bike).to eq bike
+	# end
 
 		describe '#release_bike' do
 
@@ -31,5 +31,13 @@ describe DockingStation do
 			expect {subject.release_bike}.to raise_error 'No bikes available'
 			end
 
+		end
+
+		describe '#dock' do
+
+			it 'raises an error when full' do
+				20.times {subject.dock Bike.new}
+				expect{subject.dock Bike.new}.to raise_error 'Docking station full'
+			end
 		end
 end
